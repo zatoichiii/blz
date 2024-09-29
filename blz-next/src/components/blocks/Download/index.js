f66d699e9bbe1472e68a1e7c37bcee1d64438aa9
zatoichiii <azovtsevnikita@mail.ru>
2024-09-29 14:03:38 +0300

    List

--- blz-next/src/components/blocks/Download/index.js
+++ blz-next/src/components/blocks/Download/index.js
@@ -20,11 +20,17 @@ const Download = () => {
       <Container>
         <Show>
           <div className={styles.inner}>
-            <div className={styles.tittle}>Посмотрите наш каталог в PDF формате</div>
+            <div className={styles.tittle}>Полезные ссылки</div>
+            <div className={styles.ssylky}>
             <a className={styles.ssylka} href="https://glz.by/images/custom/glz2024catalog.pdf?" target="_blank" rel="noopener noreferrer" >
             <div className={styles.desc}>Каталог GLZ 2024-2025</div>
             <button>Скачать</button>
             </a>
+            <a className={styles.ssylka} href="https://docs.google.com/document/d/1W65Xs9dOtNBPZE7qlWY4PqM0wOOjhi5qERRt3_jNTUM" target="_blank" rel="noopener noreferrer" >
+            <div className={styles.desc}>Опросный лист</div>
+            <button>Скачать</button>
+            </a>
+            </div>
           </div>
         </Show>
       </Container>
